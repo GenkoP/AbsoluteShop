5b417512e9641ef346e55338ebb5259b39756420
GenkoP <genko_petrov@hotmail.com>
2014-12-11 21:55:37 +0200

    Updated home page ui style good  . Facebook like and share buttons is not updated , because jquery can not find click event on button

--- public/app/controllers/home/homeController.js
+++ public/app/controllers/home/homeController.js
@@ -2,24 +2,32 @@
 
 'use strict';
 
-app.controller('HomeController', 
+app.controller('HomeController',
 
-    function($scope , CashedPromotion, CashedImages , InfoResource) {
+    function($scope, CashedPromotion, CashedImages, InfoResource) {
 
-        $scope.map = {center: {latitude: 43.214873, longitude: 27.921018 }, zoom: 16 };
-        $scope.marker = {
-        	id: 0,
-        	coords: {
-        		latitude:43.214873,
-        		longitude:27.921018
-        	}
+        $scope.map = {
+            center: {
+                latitude: 43.214873,
+                longitude: 27.921018
+            },
+            zoom: 16
         };
-        $scope.options = {scrollwheel: false};
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: 43.214873,
+                longitude: 27.921018
+            }
+        };
+        $scope.options = {
+            scrollwheel: false
+        };
 
         $scope.promotions = CashedPromotion.query();
 
         $scope.homeimage = CashedImages.query();
 
         $scope.compInfo = InfoResource.query();
-    
-});
+
+    });
